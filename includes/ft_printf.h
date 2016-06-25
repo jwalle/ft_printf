8e675df002edde05e21a843b124fe6ee89c4d5c1
Julien WALLE <jwalle@e2r6p18.42.fr>
2016-06-25 21:56:29 +0200

    Clean Makefile

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -14,7 +14,6 @@
 # define FT_PRINTF_H
 # include <stdarg.h>
 # include <stdint.h>
-# include <stdio.h>
 # include <limits.h>
 # include <wchar.h>
 # include <stdlib.h>
@@ -27,88 +26,84 @@
 # define LENGTH "hljz"
 # define SPECIFIER "sSpdDioOuUxXcC%"
 
-// D = ld, O = lo, U = lu.
-
-typedef	struct		s_flags
+typedef	struct	s_flags
 {
-	int				minus;
-	int				plus;
-	int				zero;
-	int				htag;
-	int				space;
-}					t_flags;
+	int			minus;
+	int			plus;
+	int			zero;
+	int			htag;
+	int			space;
+}				t_flags;
 
-typedef	struct		s_length
+typedef	struct	s_length
 {
-	int				no;
-	int				l;
-	int				ll;
-	int				h;
-	int				hh;
-	int				j;
-	int				z;
-}					t_length;
+	int			no;
+	int			l;
+	int			ll;
+	int			h;
+	int			hh;
+	int			j;
+	int			z;
+}				t_length;
 
-typedef	struct		s_arg
+typedef	struct	s_arg
 {
-	t_flags			*flags;
-	t_length		*length;
-	int				width;
-	int				precision;
-	int				precision_null;
-	char			specifier;
-	int				hex;
-	int				is_unsigned;
-}					t_arg;
+	t_flags		*flags;
+	t_length	*length;
+	int			width;
+	int			precision;
+	int			precision_null;
+	char		specifier;
+	int			hex;
+	int			is_unsigned;
+}				t_arg;
 
-typedef	struct		s_env
+typedef	struct	s_env
 {
-	int				ret;
-}					t_env;
+	int			ret;
+}				t_env;
 
-//int	ft_printf(const char *format, ...) __attribute__((format(printf,1,2)));
-
-int		ft_printf(const char *format, ...);
-char	*ft_strndup(char *str, int i);
-int		parse(const char *format, t_arg *arg, va_list list);
-int		parse_two(const char *format, t_arg *arg, int n);
-void	ft_atoi_hex(void *ptr);
-void	arg_is_int(t_env *e, t_arg *arg, va_list list);
-void	arg_is_u(t_env *e, t_arg *arg, va_list list);
-void	arg_is_c(t_env *e, t_arg *arg, va_list list);
-void	arg_is_string(t_env *e, t_arg *arg, va_list list);
-void	arg_is_p(t_env *e, t_arg *arg, va_list list);
-void	init_length(t_length *length);
-void	init_arg(t_arg *arg);
-int		ft_int_octal_len(unsigned long long n);
-int		ft_int_hex_len(unsigned long long n);
-int		ft_intlen(int n);
-int		ft_ll_intlen(unsigned long long n);
-int		ft_parse_len(unsigned long long n, char spe);
-void	ft_putllnbr(t_env *e, long long int n);
-void	parse_number(t_env *e, long long number, char spe);
-int		format_output(t_env *e, int len, int signe, t_arg *arg);
-int		format_left_width(t_env *e, t_arg *arg, int len, int signe);
-int		print_hex(t_env *e, char spe);
-void	ft_putnbr_octal(t_env *e, unsigned long long number);
-void	ft_putnbr_hexa(t_env *e, unsigned long long number, char spe);
-void	ft_putllunbr(t_env *e, unsigned long long int n);
-void	ft_putchar_ret(t_env *e, char c);
-void	ft_putstr_ret(t_env *e, const char *s);
-void	print_char(t_env *e, t_arg *arg, char c);
-
-size_t	ft_strlen_ptf(const char *s);
-int		ft_atoi_ptf(const char *str);
-void	ft_bzero_ptf(void *s, size_t n);
-int		ft_isdigit_ptf(int c);
-void	ft_putstr_ptf(const char *s);
-char	*ft_strchr_ptf(const char *s, int c);
-int		ft_strcmp_ptf(const char *s1, const char *s2);
-char	*ft_strncpy_ptf(char *dest, const char *src, size_t n);
-char	*ft_strnew_ptf(size_t size);
-void	arg_is_wchar(t_env *e, wchar_t w);
-void	print_wstr(t_env *e, wchar_t *w_str, t_arg *arg);
-void	arg_is_wstr(t_env *e, wchar_t *w_str);
-int		get_wchar_len(wchar_t c);
+int				ft_printf(const char *format,
+					...) __attribute__((format(printf,1,2)));
+char			*ft_strndup(char *str, int i);
+int				parse(const char *format, t_arg *arg, va_list list);
+int				parse_two(const char *format, t_arg *arg, int n);
+void			ft_atoi_hex(void *ptr);
+void			arg_is_int(t_env *e, t_arg *arg, va_list list);
+void			arg_is_u(t_env *e, t_arg *arg, va_list list);
+void			arg_is_c(t_env *e, t_arg *arg, va_list list);
+void			arg_is_string(t_env *e, t_arg *arg, va_list list);
+void			arg_is_p(t_env *e, t_arg *arg, va_list list);
+void			init_length(t_length *length);
+void			init_arg(t_arg *arg);
+int				ft_int_octal_len(unsigned long long n);
+int				ft_int_hex_len(unsigned long long n);
+int				ft_intlen(int n);
+int				ft_ll_intlen(unsigned long long n);
+int				ft_parse_len(unsigned long long n, char spe);
+void			ft_putllnbr(t_env *e, long long int n);
+void			parse_number(t_env *e, long long number, char spe);
+int				format_output(t_env *e, int len, int signe, t_arg *arg);
+int				format_left_width(t_env *e, t_arg *arg, int len, int signe);
+int				print_hex(t_env *e, char spe);
+void			ft_putnbr_octal(t_env *e, unsigned long long number);
+void			ft_putnbr_hexa(t_env *e, unsigned long long number, char spe);
+void			ft_putllunbr(t_env *e, unsigned long long int n);
+void			ft_putchar_ret(t_env *e, char c);
+void			ft_putstr_ret(t_env *e, const char *s);
+void			print_char(t_env *e, t_arg *arg, char c);
+size_t			ft_strlen_ptf(const char *s);
+int				ft_atoi_ptf(const char *str);
+void			ft_bzero_ptf(void *s, size_t n);
+int				ft_isdigit_ptf(int c);
+void			ft_putstr_ptf(const char *s);
+char			*ft_strchr_ptf(const char *s, int c);
+int				ft_strcmp_ptf(const char *s1, const char *s2);
+char			*ft_strncpy_ptf(char *dest, const char *src, size_t n);
+char			*ft_strnew_ptf(size_t size);
+void			arg_is_wchar(t_env *e, wchar_t w);
+void			print_wstr(t_env *e, wchar_t *w_str, t_arg *arg);
+void			arg_is_wstr(t_env *e, wchar_t *w_str);
+int				get_wchar_len(wchar_t c);
 
 #endif
